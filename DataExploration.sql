bb1a477a35de42c8954c6900563a60116790f297
Amari Keo <129122755+amarikeo@users.noreply.github.com>
2023-04-27 16:29:36 -0500

    Update DataExploration.sql

--- DataExploration.sql
+++ DataExploration.sql
@@ -6,9 +6,11 @@ Skills used: Joins, CTE's, Temp Tables, Windows Functions, Aggregate Functions, 
 */
 
 -- SELECTING THE DATA WE'RE GOING TO BE USING --
+-- Continent can not be NULL, will return continent values for location.
 
 SELECT location, date, total_cases, new_cases, total_deaths, population
 FROM pfcovid..coviddeaths$
+WHERE continent IS NOT NULL
 ORDER BY 1,2
 ;
 
@@ -19,8 +21,10 @@ ORDER BY 1,2
 
 SELECT location, date, total_cases, total_deaths, (total_deaths/total_cases)*100 AS DeathPercentage
 FROM pfcovid..coviddeaths$
+WHERE continent IS NOT NULL
 ORDER BY 1,2
 ;
+
 
 
 
@@ -34,6 +38,7 @@ ORDER BY 1,2
 ;
 
 
+
 -- Looking at the Total Cases vs Population in US.
 -- Displays percentage of population who conctracted COVID-19.
 
@@ -44,11 +49,47 @@ ORDER BY 1,2
 ;
 
 
+
 -- Examining the Country with the Highest Infection Rate in relation to its Population
 
 SELECT location, population, MAX(total_cases) AS HighestInfectionCount, MAX((total_cases/population))*100
 AS PercentPopulationInfected
 FROM pfcovid..coviddeaths$
+WHERE continent IS NOT NULL
 GROUP BY location, population
 ORDER BY PercentPopulationInfected DESC
+;
+
+
+
+-- Showing Countries with Highest Death Count 
+
+SELECT location, MAX(CAST(total_deaths AS INT)) AS TotalDeathCount
+FROM pfcovid..coviddeaths$
+WHERE continent IS NOT NULL
+GROUP BY location
+ORDER BY TotalDeathCount DESC
+;
+
+
+
+-- Showing the Continents with Highest Death Count
+
+SELECT continent, MAX(CAST(total_deaths AS INT)) AS TotalDeathCount
+FROM pfcovid..coviddeaths$
+WHERE continent IS NOT NULL
+GROUP BY continent
+ORDER BY TotalDeathCount DESC
+;
+
+
+
+-- GLOBAL NUMBERS
+
+SELECT date, SUM(new_cases) AS total_cases, SUM(CAST(new_deaths AS INT)) AS total_deaths, SUM(CAST(new_deaths AS INT))/
+SUM(new_cases)*100 AS DeathPercentage
+FROM pfcovid..coviddeaths$
+WHERE continent IS NOT NULL
+GROUP BY date
+ORDER BY 1,2
 ;
